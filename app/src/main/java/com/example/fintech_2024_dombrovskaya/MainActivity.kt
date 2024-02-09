0edbf5e6cfe4bbff02c068f99d3b1b9f5a9a497f
Marii <mariiaiivanovva@yandex.ru>
2024-02-09 23:55:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fintech_2024_dombrovskaya/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fintech_2024_dombrovskaya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
